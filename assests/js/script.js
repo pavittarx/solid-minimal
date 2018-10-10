9f30cf9a2773cf566373dfbf007e34ff558b0366
Pavittar Singh <pavittarx@users.noreply.github.com>
2018-10-10 22:17:49 +0530

    finished authentication features

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,21 +1,23 @@
-// refernce to the form getting IDP details.
-const idpForm = document.getElementById('idp-form');
-// a button to log the user out
-const logoutButton = document.getElementById('logout-button');
 
 // Tracks if the user is logged in or out.
 solid.auth.trackSession(session => {
+  const loginForm = document.getElementById('login-form');
+  const loginContainer = document.getElementById('login-section');
+  const logoutButton = document.getElementById('logout-button');
   if (!session) {
-    
-    idpForm.style.display = "block";
+
+    registerLoginEvents(loginForm, logoutButton);
+    attachDropdownEvents();
+    loginContainer.style.display = "block";
     logoutButton.style.display = "none";
 
   } else {
 
-    console.log(`The user is ${session.webId}`);
-
-    idpForm.style.display = "none";
+    registerLogoutEvents(loginForm, logoutButton);
+    removeDropdownEvents();
+    loginContainer.style.display = "none";
     logoutButton.style.display = "inline-block";
+
   }
 
 });
@@ -32,17 +34,68 @@ async function logout() {
   await solid.auth.logout();
 }
 
-// calls the login function, when the form is submitted
-idpForm.addEventListener("submit", (event) => {
-  // prevents form from submission  
-  event.preventDefault();
-  // gets the inputted IDP information
-  let idp = document.getElementById('idp-uri').value;
-  // calls the login function
-  login(idp);
-});
+async function popupLogin() {
+  let session = await solid.auth.currentSession();
+  let popupUri = 'https://solid.community/common/popup.html';
+  if (!session)
+    session = await solid.auth.popupLogin({ popupUri });
+  alert(`Logged in as ${session.webId}`);
+}
 
-// attaches an event listener to logout button.
-logoutButton.addEventListener('click', () => {
-  logout();
-})
+
+function registerLoginEvents(loginForm, logoutButton) {
+  // calls the login function, when the form is submitted
+  loginForm.addEventListener("submit", (event) => {
+    // prevents form from submission  
+    event.preventDefault();
+    // gets the inputted IDP information
+    let idp = document.getElementById('idp-uri').value;
+    // calls the login function
+    login(idp);
+  });
+
+   logoutButton.removeEventListener('click',()=>{
+    console.log('Logout Listener Removed');
+  })
+
+  document.getElementById('popup-login-button').addEventListener('click',()=>{
+    popupLogin();
+  })
+}
+
+function registerLogoutEvents(loginForm, logoutButton) {
+  // attaches an event listener to logout button.
+  logoutButton.addEventListener('click', () => {
+    logout();
+  });
+
+  loginForm.removeEventListener('submit', ()=>{
+    console.log('submit', ()=>{
+      console.log('Submit Listener Removed');
+    })
+  })
+}
+
+/* adds the URI of identity providers to be used by login() */
+function attachDropdownEvents() {
+  let idpProviders = document.querySelectorAll('.idp-provider > span');
+  let idpUri = document.getElementById('idp-uri');
+
+  console.log(idpProviders[0].textContent);
+
+  for (let i = 0; i < idpProviders.length; i++) {
+    idpProviders[i].addEventListener('click', () => {
+      idpUri.value = idpProviders[i].textContent.trim();
+    })
+  }
+}
+
+function removeDropdownEvents() {
+  let idpProviders = document.querySelectorAll('.idpProviders > span');
+  for (let i = 0; i < idpProviders.length; i++) {
+    idpProviders[i].removeEventListeners('click', () => {
+      console.log('Login Event removed for ', idpProviders[i].textContent);
+    })
+  }
+}
+
